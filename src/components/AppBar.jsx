0b841397d90d6f6a95bca7ef7301fea28396f67a
denis <denis.kuznetsov@aalto.fi>
2023-08-28 18:01:43 +0300

    exercise 10.4-10.7

---
+++ src/components/AppBar.jsx
@@ -0,0 +1,33 @@
+import { View, StyleSheet, ScrollView } from "react-native";
+import Constants from "expo-constants";
+import theme from "../theme";
+import AppBarTab from "./AppBarTab";
+
+const styles = StyleSheet.create({
+  container: {
+    paddingTop: Constants.statusBarHeight + 15,
+    backgroundColor: theme.colors.textPrimary,
+    flexDirection: "row",
+    justifyContent: "flex-start",
+    width: "100%",
+    paddingHorizontal: 10,
+    paddingBottom: 15,
+  },
+});
+
+const AppBar = () => {
+  return (
+    <View style={styles.container}>
+      <ScrollView horizontal>
+        <AppBarTab text={"Repositories"} to="/" />
+        <AppBarTab text={"Sign in"} to="/signIn" style={{ marginStart: 5 }} />
+        <AppBarTab text={"Sign in"} to="/signIn" style={{ marginStart: 5 }} />
+        <AppBarTab text={"Sign in"} to="/signIn" style={{ marginStart: 5 }} />
+        <AppBarTab text={"Sign in"} to="/signIn" style={{ marginStart: 5 }} />
+        <AppBarTab text={"Sign in"} to="/signIn" style={{ marginStart: 5 }} />
+      </ScrollView>
+    </View>
+  );
+};
+
+export default AppBar;
